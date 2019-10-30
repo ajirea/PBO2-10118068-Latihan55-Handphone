1c6df1d398980159bc023edf6733e1015a492883
Satria Aji Putra <satriamaxt@gmail.com>
2019-10-31 05:08:05 +0700

    Initial commit

---
+++ src/pbo2/pkg10118068/latihan55/handphone/BlackBerry.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo2.pkg10118068.latihan55.handphone;
+
+/**
+ *	
+ * @author
+ * NAMA     : Satria Aji Putra
+ * KELAS    : IF-2
+ * NIM      : 10118068
+ * Deskripsi Program : Membuat program berbasis object untuk mendapatkan
+ * informasi handphone
+ */
+public class BlackBerry extends Handphone {
+    private String pinBB;
+
+    public BlackBerry(String man, String os, String model, int harga) {
+        super(man, os, model, harga);
+    }
+
+    public String getPinBB() {
+        return pinBB;
+    }
+
+    public void setPinBB(String pinBB) {
+        this.pinBB = pinBB;
+    }
+}
---
+++ src/pbo2/pkg10118068/latihan55/handphone/Tester.java
@@ -0,0 +1,39 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo2.pkg10118068.latihan55.handphone;
+
+/**
+ *	
+ * @author
+ * NAMA     : Satria Aji Putra
+ * KELAS    : IF-2
+ * NIM      : 10118068
+ * Deskripsi Program : Membuat program berbasis object untuk mendapatkan
+ * informasi handphone
+ */
+public class Tester {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Android android = new Android("Android", "Android", "Grand", 3000000);
+        android.setKeyStore("234ibfd3840fo");
+        android.displayProduct();
+        System.out.println("Android Key Store : " + android.getKeyStore() + "\n");
+        
+        BlackBerry bb = new BlackBerry("BlackB", "RIM", "Curve", 2000000);
+        bb.setPinBB("BHS249");
+        bb.displayProduct();
+        System.out.println("PIN : " + bb.getPinBB() + "\n");
+        
+        WindowsPhone wp = new WindowsPhone("Nokia", "Win8", "Lumia", 1000000);
+        wp.setWpKeyStore("UUUQIJWORJ");
+        wp.displayProduct();
+        System.out.println("Wp Key Store : " + wp.getWpKeyStore() + "\n");
+    }
+    
+}
